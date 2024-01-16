d468a32f7a35574218817918f7522999bd5e7d3f
DIP05I <fejer.csa@gmail.com>
2024-01-16 17:47:15 +0100

    Elso

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#  teszt
+import utanfuto
+
+
+
+print("1. kölcsönzés")
+print("2. lemondás")
+print("3. listázás")
+
+
+
--- utanfuto.py
+++ utanfuto.py
@@ -1,7 +1,12 @@
 class Utanfuto:
-    def __init__(self,tipus,ar,teherbiras,kolcsonozheto = "igen"):
+    def __init__(self,tipus,ar,teherbiras,rendszam,kolcsonozheto = "igen"):
         self.tipus = tipus
         self.ar = ar
         self.teherbiras = teherbiras
+        self.rendszam = rendszam
+
+
+
+
 
 
